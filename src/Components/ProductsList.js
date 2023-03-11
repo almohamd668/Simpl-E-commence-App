6e0daa210c92035c5851dbdd14a1ec5690d89e60
username <mohmtmpp@gmail.com>
2023-03-11 14:38:40 +0300

    add categories filter

--- src/Components/ProductsList.js
+++ src/Components/ProductsList.js
@@ -1,41 +1,59 @@
 import { useEffect, useState } from "react";
-import Product from './Product';
+import Product from "./Product";
 
 const ProductsList = () => {
-
   const apiUrl = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
 
-  function getData(api="") {
-    let setd ;
-    api === "" ? setd = setProducts :setd = setCategories;
+  function getData(api = "") {
+    let setD;
+    api === "categories" ? (setD = setCategories) : (setD = setProducts);
 
     fetch(`${apiUrl}/${api}`)
-    .then((res) => res.json())
-    .then((data) => setd(data))
+      .then((res) => res.json())
+      .then((data) => setD(data));
   }
-  
+
   useEffect(() => {
     getData();
     getData("categories");
   }, []);
 
-
   return (
     <>
       <h2 className="text-center">Our Products</h2>
+      <button
+        onClick={() => {
+          getData()
+        }}
+        className="btn btn-primary m-3"
+      >
+        All
+      </button>
+      {categories.map((cat) => {
+        return (
+          <button
+            key={cat}
+            onClick={() => {
+              getData(`category/${cat}`);
+            }}
+            className="btn btn-primary m-3"
+          >
+            {cat}
+          </button>
+        );
+      })}
+
       <div className="row">
         {products.map((product) => {
           return (
             <div className="col-3" key={product.id}>
-            <Product product={product} showButton={true} />
-             <h1>{product.id}</h1>
-          </div>
+              <Product product={product} showButton={true} />
+              <h1>{product.id}</h1>
+            </div>
           );
-        } )}
-      
-       
+        })}
       </div>
     </>
   );
