1c59372425534400288e8029c626ab5bfad8d492
username <mohmtmpp@gmail.com>
2023-03-14 06:42:36 +0300

    n

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,7 @@ import Notfound from './Notfound';
 
 export const rout = createBrowserRouter([
  {
-  path:"/Simple-E-commence-App",
+  path:"Simple-E-commence-App",
   element:<App/>,
   children: [
     {
