446aae9ad5cb4051a4ecf621878ea20f5977bf06
username <mohmtmpp@gmail.com>
2023-03-14 15:29:03 +0300

    routes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,12 +30,12 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
+                <Link className="nav-link " aria-current="page" to="">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <Link className="nav-link" to="about">
                   About
                 </Link>
               </li>
